683c8a4e29691f796cece890a4dd2d14cd40f5c9
Alejandro Rodriguez <alejandro.rodriguez@iberant.es>
2020-02-10 15:30:10 -0500

    Refactoring and jompi's code deleted so that we can make use of the project tree

--- kdtools/models.py
+++ kdtools/models.py
@@ -4,67 +4,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 
-import kdtools as kd
+import kdtools as kdtools
 
-
-class BasicSequenceTagger(nn.Module):
-    def __init__(
-        self,
-        *,
-        char_vocab_size,
-        char_embedding_dim,
-        padding_idx,
-        char_repr_dim,
-        word_repr_dim,
-        postag_repr_dim,
-        token_repr_dim,
-        num_labels,
-    ):
-        super().__init__()
-
-        # List[Char] -> List[Vector]
-        self.char2embed = nn.Embedding(
-            num_embeddings=char_vocab_size,
-            embedding_dim=char_embedding_dim,
-            padding_idx=padding_idx,
-        )
-
-        # List[Vector] -> List[Vector]
-        self.char_bilstm = kd.BiLSTMEncoder(
-            input_size=char_embedding_dim,
-            hidden_size=char_repr_dim // 2,
-            num_layers=1,
-            batch_first=True,
-        )
-
-        # List[Token] -> List[Vector]
-        self.token_bilstm = nn.LSTM(
-            input_size=char_repr_dim + word_repr_dim + postag_repr_dim,
-            hidden_size=token_repr_dim // 2,
-            num_layers=1,
-            batch_first=True,
-            bidirectional=True,
-        )
-
-        # Vector -> Category
-        self.token2tag = nn.Linear(token_repr_dim, num_labels)
-
-    def forward(self, sentence):
-        # input
-        chars, words, postags = sentence
-
-        # character input
-        char_embeds = self.char2embed(chars)
-        char_reprs, _ = self.char_bilstm(char_embeds)
-
-        # token representation
-        tokens = torch.cat((char_reprs, words, postags), 1).unsqueeze(0)
-        token_reprs, _ = self.token_bilstm(tokens)
-
-        # output
-        out = self.token2tag(token_reprs)
-        return out
-        
 class ChainCRF(nn.Module):
     def __init__(self, input_size, num_labels, bigram=True):
         super(ChainCRF, self).__init__()
