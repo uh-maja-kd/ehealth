5e4e905e33eea765625bffb80b9a8f6f0ba27ab0
Ernesto <ernesto@divineslns.com>
2020-02-12 12:37:21 -0500

    Add example of use of BetoEncoder

--- kdtools/layers.py
+++ kdtools/layers.py
@@ -23,4 +23,24 @@ class BetoEncoder(nn.Module):
         return output
         
 
-        return output, hidden
+def testBetoLayer():
+    model_path = '/pytorch'
+
+    tokenizer = BertTokenizer.from_pretrained(model_path, do_lower_case=False)
+    config, unused_kwargs = BertConfig.from_pretrained(model_path, output_attention=True,
+                                                    foo=False, return_unused_kwargs=True)
+    model = BertModel(config).from_pretrained(model_path)
+    model.eval()
+
+    text = "[CLS] Para solucionar los [MASK] de Chile, el presidente debe [MASK] de inmediato. [SEP]"
+    masked_indxs = (4,11)
+
+    tokens = tokenizer.tokenize(text)
+    indexed_tokens = tokenizer.convert_tokens_to_ids(tokens)
+    tokens_tensor = torch.tensor([indexed_tokens])
+
+    output = model(tokens_tensor)
+    output[0].shape
+
+if __name__ == "__main__":
+    testBetoLayer()
