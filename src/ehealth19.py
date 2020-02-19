9cbabb0d8652520eb678cbb0bef82e2bcd9ad60b
Ernesto <ernesto@divineslns.com>
2020-02-19 16:03:13 -0500

    Created prototype of train method for Beto-BiLSTM-Model

--- src/ehealth19.py
+++ src/ehealth19.py
@@ -7,7 +7,7 @@ from scripts.submit import Algorithm
 from scripts.utils import Collection
 
 from kdtools.datasets import RelationsDependencyParseActionsDataset
-from kdtools.models import BiLSTMDoubleDenseOracleParser
+from kdtools.models import BiLSTMDoubleDenseOracleParser, 
 
 
 class UHMajaModel(Algorithm):
@@ -18,12 +18,54 @@ class UHMajaModel(Algorithm):
     def run(self, collection: Collection, *args, taskA: bool, taskB: bool, **kargs):
         return super().run(collection, *args, taskA=taskA, taskB=taskB, **kargs)
 
-    def train(self, collection: Collection, n_epochs=100):
-        self.model_taskA = self.train_taskA(collection, n_epochs)
+    def train(self, collection: Collection, config, n_epochs=100):
+        self.model_taskA = self.train_taskA(collection, config, n_epochs)
         self.model_taskB = self.train_taskB(collection, n_epochs)
 
-    def train_taskA(self, collection, n_epochs=100):
-        pass
+    def train_taskA(self, collection, config, n_epochs=100):
+        dataset = EntityExtracionDataSet(collection)
+        model = BiRecurrentConvCRF(300, config.mode, config.hidden_size, config.out_features, config.num_layers, 2, 
+                                    config.p_in, config.p_out, config.p_rnn, config.bigram, config.activation)
+
+        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        criterion_act = CrossEntropyLoss()
+        criterion_rel = CrossEntropyLoss()
+
+        for epoch in range(n_epochs):
+            correct = 0
+            total = 0
+            running_loss_act = 0.0
+            running_loss_rel = 0.0
+
+            for data in tqdm(dataset):
+                *X, y_act, y_rel = data
+                X = [x.view(1,-1,10) for x in X]
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                output_act, output_rel = model(X)
+
+                loss_act = criterion_act(output_act, y_act)
+                loss_act.backward(retain_graph = True)
+
+                loss_rel = criterion_rel(output_rel, y_rel)
+                loss_rel.backward()
+
+                optimizer.step()
+
+                running_loss_act += loss_act.item()
+                running_loss_rel += loss_rel.item()
+
+                predicted_act = torch.argmax(output_act, -1)
+                predicted_rel = torch.argmax(output_rel, -1)
+                total += 1
+                correct += int(predicted_act == y_act and predicted_rel == y_rel)
+
+            print(f"[{epoch + 1}] loss_act: {running_loss_act / len(dataset) :0.3}")
+            print(f"[{epoch + 1}] loss_rel: {running_loss_rel / len(dataset) :0.3}")
+            print(f"[{epoch + 1}] accuracy: {correct / total}")
+
+        return model
 
     def train_taskB(self, collection: Collection, n_epochs=100):
         dataset = RelationsDependencyParseActionsDataset(collection)
