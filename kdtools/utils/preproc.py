a80b895ea6eeb4abcabb2a7b3cae734aec56c7c9
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2020-02-27 16:43:31 -0500

    Fix ale snaky things

--- kdtools/utils/preproc.py
+++ kdtools/utils/preproc.py
@@ -1,19 +1,18 @@
 def _get_spans(self, sentence: str):
-    spans = []
-        begun = False
-        start = None
-        punct = ".,;:()-\"\""
-        for i, c in enumerate(sentence):
-            if not begun and c not in " " + punct:
-                begun = True
-                start = i
-            if begun and c in " " + punct:
-                begun = False
-                spans.append((start, i))
-                if c in punct:
-                    spans.append((i, i))
+    spans, begun, start = [], False, None
+    punct = '.,;:()-""'
+    for i, c in enumerate(sentence):
+        if not begun and c not in " " + punct:
+            begun = True
+            start = i
+        if begun and c in " " + punct:
+            begun = False
+            spans.append((start, i))
+            if c in punct:
+                spans.append((i, i))
 
-        return spans
+    return spans
 
-def _get_spacy_vector(self, word: str, lang = 'spanish' :str):
+
+def _get_spacy_vector(self, word: str, lang: str = "spanish"):
     pass
